61c153bb8a734d446209b245802aaadce4197e4a
Ivan Gudak <ihudak@gmail.com>
2023-02-02 23:28:26 +0100

    kubernitize services

---
+++ restart.sh
@@ -0,0 +1,10 @@
+#!bash
+kubectl apply -f k8s-clients.yaml
+kubectl apply -f k8s-books.yaml
+kubectl apply -f k8s-cart.yaml
+kubectl apply -f k8s-storage.yaml
+kubectl apply -f k8s-orders.yaml
+kubectl apply -f k8s-payment.yaml
+kubectl apply -f k8s-dynapay.yaml
+kubectl apply -f k8s-ingest.yaml
+kubectl apply -f k8s-bookstore.yaml
